341c1cac7df8d2da9c4ca5d3ea95cb9bd7df6d92
kofori00 <kofori00@gmail.com>
2019-05-27 10:27:53 +0200

    working scraper

--- epex/epex/spiders/EpeX.py
+++ epex/epex/spiders/EpeX.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import datetime
 
 class EpexSpider(scrapy.Spider):
     name = 'EpeX'
     allowed_domains = ['www.epexspot.com/en/market-data/intradaycontinuous/intraday-table/2019-05-25/DE']
-    start_urls = ['http://www.epexspot.com/en/market-data/intradaycontinuous/intraday-table/2019-05-25/DE/']
- 
+    start_urls = ["https://www.epexspot.com/en/market-data/intradaycontinuous/intraday-table/" + str(datetime.date.today())+"/DE"]
+    features=[Low, High, Last(€/MWh), Weight_Avg(€/MWh), Index(€/MWh), ID3_Price(€/MWh), ID1_Price(€/MWh), BuyVol(MW), SellVol(MW), Low2(€/MWh), High2(€/MWh), Last2(€/MWh), WeightAvg2(€/MWh), Index2(€/MWh), ID3Price2(€/MWh), ID1Price2(€/MWh), BuyVol2(MW) 	SellVol2]
+    
     def parse (self,response):
-        date = response.xpath("id('content')/div/table/tbody/tr[1]/th[contains(@class, 'date')]/text()").get()
-        price_list = response.xpath("id('content')/div/table/tbody/tr/th[contains(@class, 'date')]/text()").get()
-        time = response.xpath("id('content')/div/table/tbody/tr/td[contains(@class, 'title')]/text()").get()
-        x = response.xpath("id('quarter_auction_table_wrapp')/table/tbody/tr[contains(@class, 'hour')]/td/text()").get()
-        y = response.xpath("id('tab_de')/table[2]/tbody/tr/td[contains(@class, 'title')]/../td/text()").get()
-        z = response.xpath("id('tab_de')/table[1]/tbody/tr/td/text()").get()
-
-        yield{'datum':date,'tijd':time,'prijzen':price_list}
+        for i,feat in enumerate(features):
+            date = response.xpath('//td[3]').extract()
+            feat = response.xpath('//'+str(td[i+4])).extract()
